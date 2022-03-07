02cdacdf7a4a33a589a8f75257339c4902689a67
ShadySamirBakheet <shadysamirbakheet@gmail.com>
2022-03-07 12:11:52 +0200

    add add docs to Repo at 12 PM on 7 mar 2022

--- lib/data_source/local/file_storage/file_storage.dart
+++ lib/data_source/local/file_storage/file_storage.dart
@@ -1,21 +1,26 @@
 import 'dart:async';
 import 'dart:io';
+import 'dart:typed_data';
+
+import 'package:dio/dio.dart';
 
 import 'package:path_provider/path_provider.dart';
 
-class CounterStorage {
-  Future<String> get _localPath async {
+class FileStorage {
+  static Future<String> get _localPath async {
     final directory = await getApplicationDocumentsDirectory();
 
     return directory.path;
   }
 
-  Future<File> _localFile(String pathToFile) async {
+  // static get _httpClient => HttpClient();
+
+  static Future<File> _localFile(String pathToFile) async {
     final path = await _localPath;
     return File('$path/pathFile');
   }
 
-  Future<File?> readFile(String pathFile) async {
+  static Future<File?> readFile(String pathFile) async {
     try {
       return await _localFile(pathFile);
     } catch (e) {
@@ -24,10 +29,20 @@ class CounterStorage {
     }
   }
 
-  Future<File?> writeCounter(String pathFile, fileData) async {
+  static Future<File?> writeCounter(String pathFile, String uri) async {
     final file = await _localFile(pathFile);
 
+    //var requset = await _httpClient.getUri(uri);
+    //var response = await requset.close();
+    //Uint8List bytes = await consolidateHttpClientResponseBytes(response);
+
+    var dio = Dio();
+
+    Uri data = Uri.file(uri);
+
+    Response<Uint8List> bytes = await dio.getUri<Uint8List>(data);
+
     // Write the file
-    return file.writeAsBytes(fileData);
+    return file.writeAsBytes(bytes.data!);
   }
 }
